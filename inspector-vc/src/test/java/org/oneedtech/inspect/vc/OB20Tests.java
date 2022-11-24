20ca615e468de08b60593918837a74b47014afee
Xavi Aracil <xaracil@1edtech.org>
2022-11-24 17:07:10 +0100

    Added simple badge class test

--- inspector-vc/src/test/java/org/oneedtech/inspect/vc/OB20Tests.java
+++ inspector-vc/src/test/java/org/oneedtech/inspect/vc/OB20Tests.java
@@ -48,6 +48,17 @@ public class OB20Tests {
 		});
 	}
 
+	@Test
+	void testSimpleBadgeClassJsonValid() {
+		assertDoesNotThrow(()->{
+			Report report = validator.run(Samples.OB20.JSON.SIMPLE_BADGECLASS.asFileResource());
+			if(verbose) PrintHelper.print(report, true);
+			assertValid(report);
+		});
+	}
+
+
+
 	@Nested
 	static class WarningTests {
 		@BeforeAll
