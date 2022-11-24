ca8050d79045ebf0691dc832c05e2d2a918deeb8
Xavi Aracil <xaracil@1edtech.org>
2022-11-24 12:01:53 +0100

    Added basic test, using test inspector

--- inspector-vc/src/test/java/org/oneedtech/inspect/vc/OB20Tests.java
+++ inspector-vc/src/test/java/org/oneedtech/inspect/vc/OB20Tests.java
@@ -3,22 +3,29 @@ package org.oneedtech.inspect.vc;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.oneedtech.inspect.test.Assertions.assertValid;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.oneedtech.inspect.core.Inspector.Behavior;
 import org.oneedtech.inspect.core.report.Report;
 import org.oneedtech.inspect.test.PrintHelper;
+import org.oneedtech.inspect.vc.util.TestOB20Inspector.TestBuilder;
 
 public class OB20Tests {
 	private static OB20Inspector validator;
 	private static boolean verbose = true;
 
 	@BeforeAll
-	static void setup() {
-		validator = new OB20Inspector.Builder()
-				.set(Behavior.TEST_INCLUDE_SUCCESS, true)
-				.set(Behavior.VALIDATOR_FAIL_FAST, true)
-				.build();
+	static void setup() throws URISyntaxException {
+		validator = new TestBuilder()
+			.add(new URI("https://www.example.org/"), "ob20/assets")
+			.set(Behavior.TEST_INCLUDE_SUCCESS, true)
+			.set(Behavior.TEST_INCLUDE_WARNINGS, true)
+			.set(Behavior.VALIDATOR_FAIL_FAST, true)
+			.set(OB20Inspector.Behavior.ALLOW_LOCAL_REDIRECTION, true)
+			.build();
 	}
 
 	@Test
@@ -29,5 +36,4 @@ public class OB20Tests {
 			assertValid(report);
 		});
 	}
-
 }
