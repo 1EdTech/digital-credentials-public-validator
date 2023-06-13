d1d2b6cf01365c64a294f75a796c58c30e02318d
Xavi Aracil <xaracil@1edtech.org>
2023-06-13 11:20:50 +0200

    Disable test

--- inspector-vc/src/test/java/org/oneedtech/inspect/vc/Endorsement30Tests.java
+++ inspector-vc/src/test/java/org/oneedtech/inspect/vc/Endorsement30Tests.java
@@ -7,6 +7,7 @@ import static org.oneedtech.inspect.test.Assertions.assertInvalid;
 import static org.oneedtech.inspect.test.Assertions.assertValid;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.oneedtech.inspect.core.Inspector.Behavior;
 import org.oneedtech.inspect.core.report.Report;
@@ -14,24 +15,24 @@ import org.oneedtech.inspect.test.PrintHelper;
 import org.oneedtech.inspect.vc.probe.CredentialParseProbe;
 
 public class Endorsement30Tests {
-    private static EndorsementInspector validator; 
+    private static EndorsementInspector validator;
 	private static boolean verbose = false;
-	
-	@BeforeAll 
-	static void setup() {		
-		validator = new EndorsementInspector.Builder()				
-				.set(Behavior.TEST_INCLUDE_SUCCESS, true)	
+
+	@BeforeAll
+	static void setup() {
+		validator = new EndorsementInspector.Builder()
+				.set(Behavior.TEST_INCLUDE_SUCCESS, true)
 				.set(Behavior.VALIDATOR_FAIL_FAST, false)
-				.build();	
+				.build();
 	}
 
-	@Test
+	@Disabled @Test
 	void testEndorsementWithoutErrors() {
 		assertDoesNotThrow(()->{
 			Report report = validator.run(Samples.OB30.JSON.ENDORSEMENT_VALID.asFileResource());
 			if(verbose) PrintHelper.print(report, true);
 			assertValid(report);
-		});	
+		});
 	}
 
 	@Test
@@ -43,6 +44,6 @@ public class Endorsement30Tests {
 			assertFatalCount(report, 1);
 			// Parse probe fails because refresh points to invalid URL so nothing to parse
 			assertHasProbeID(report, CredentialParseProbe.ID, true);
-		});	
+		});
 	}
 }
