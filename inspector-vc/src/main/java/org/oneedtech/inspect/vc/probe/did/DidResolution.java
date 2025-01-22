0b37e6855be14c718cd8343abbe2f269b96c640a
Xavi Aracil <xaracil@1edtech.org>
2025-01-22 09:56:33 +0100

    Add publicKeyJwk field to DidResolution class and update builder

--- inspector-vc/src/main/java/org/oneedtech/inspect/vc/probe/did/DidResolution.java
+++ inspector-vc/src/main/java/org/oneedtech/inspect/vc/probe/did/DidResolution.java
@@ -2,15 +2,21 @@ package org.oneedtech.inspect.vc.probe.did;
 
 public class DidResolution {
     private final String publicKeyMultibase;
+    private final String publicKeyJwk;
     private final String controller;
 
     DidResolution(Builder builder) {
         this.publicKeyMultibase =  builder.publicKeyMultibase;
+        this.publicKeyJwk = builder.publicKeyJwk;
         this.controller = builder.controller;
     }
 
     public String getPublicKeyMultibase() {
         return publicKeyMultibase;
+    }
+
+    public String getPublicKeyJwk() {
+        return publicKeyJwk;
     }
 
     public String getController() {
@@ -24,10 +30,16 @@ public class DidResolution {
     // builder
     public static class Builder {
         String publicKeyMultibase;
+        String publicKeyJwk;
         String controller;
 
         public Builder publicKeyMultibase(String publicKeyMultibase) {
             this.publicKeyMultibase = publicKeyMultibase;
+            return this;
+        }
+
+        public Builder publicKeyJwk(String publicKeyJwk) {
+            this.publicKeyJwk = publicKeyJwk;
             return this;
         }
 
