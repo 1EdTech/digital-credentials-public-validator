9ddc715cfdafe903b4d68e1bf95e3f169144d38a
Xavi Aracil <xaracil@1edtech.org>
2025-11-06 14:42:25 +0100

    refactor: simplify JsonLDValidationProbe to accept raw JSON input

--- inspector-vc/src/main/java/org/oneedtech/inspect/vc/jsonld/probe/JsonLDValidationProbe.java
+++ inspector-vc/src/main/java/org/oneedtech/inspect/vc/jsonld/probe/JsonLDValidationProbe.java
@@ -5,23 +5,21 @@ import java.io.StringReader;
 import org.oneedtech.inspect.core.probe.Probe;
 import org.oneedtech.inspect.core.probe.RunContext;
 import org.oneedtech.inspect.core.report.ReportItems;
-import org.oneedtech.inspect.vc.Credential;
-import org.oneedtech.inspect.vc.jsonld.JsonLdGeneratedObject;
+import org.oneedtech.inspect.vc.util.CachingDocumentLoader;
 
 import foundation.identity.jsonld.JsonLDObject;
 import foundation.identity.jsonld.validation.Validation;
 
-public class JsonLDValidationProbe extends Probe<Credential> {
-    private final JsonLdGeneratedObject jsonLdObject;
+public class JsonLDValidationProbe extends Probe<String> {
 
-    public JsonLDValidationProbe(JsonLdGeneratedObject jsonLdObject) {
+    public JsonLDValidationProbe() {
         super();
-        this.jsonLdObject = jsonLdObject;
     }
 
     @Override
-    public ReportItems run(Credential crd, RunContext ctx) throws Exception {
-        JsonLDObject jsonLd = JsonLDObject.fromJson(new StringReader(jsonLdObject.getJson()));
+    public ReportItems run(String json, RunContext ctx) throws Exception {
+        JsonLDObject jsonLd = JsonLDObject.fromJson(new StringReader(json));
+        jsonLd.setDocumentLoader(new CachingDocumentLoader());
         try {
             Validation.validate(jsonLd);
             return success(this, ctx);
